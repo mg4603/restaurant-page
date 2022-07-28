9b4a0cd899c35299b1c9f37fb3e1d64fbfd4b6d0
Michael George <michael.g.george@gmail.com>
2022-07-29 05:24:28 +0530

    Complete bare-bones menu

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,14 @@
 import {createImage, createH2, createParagraph, createDiv} from './helper';
+import belgianFries from './images/belgian-fries.jpg';
+import tatterTots from './images/tatter-tots.jpg';
+import standardCut from './images/standard-cut.jpg';
+import garlicFries from './images/garlic-fries.jpg';
+import curlyFries from './images/curly-fries.jpg';
+import cheeseFries from './images/cheese-fries.jpg';
+import potatoWedges from './images/potato-wedges.jpg';
+import potatoTornado from './images/potato-tornado.jpg';
+import steakFries from './images/steak-fries.jpg';
+import chips from './images/chips.jpg';
 
 function createItem(img, alt, title, text, credit){
     const card = createDiv();
@@ -24,88 +34,69 @@ function menu(){
             `Irregularly sliced, double-fried potatoes served 
             in a cone with mayonnaise and other condiments.`,
             `Photo by Katja Rooke on Unsplash`));
+    
     menuCard.appendChild(createItem(tatterTots,
             'tatter tots',
             'Tatter Tots',
             `Grated parboiled potatoes, formed into small nuggets
             and fried.`,
-            ));
+            `Photo by Nathan Dumlao on Unsplash`));
+
     menuCard.appendChild(createItem(standardCut,
             'standard cut',
             'Standard Cut',
-            `Potatoes evenly cut medium-thin, fried once.`));
+            `Potatoes evenly cut medium-thin, fried once.`,
+            `Photo by Fernanda Martinez on Unsplash`));
+    
     menuCard.appendChild(createItem(garlicFries,
             'garlic fries',
-            'Garlic Fries'
+            'Garlic Fries',
             `Any cut of fries sprinkled with minced garlic and
-            chopped parsley.`));
+            chopped parsley.`,
+            `Photo by Jay Gajjar on Unsplash`));
+
     menuCard.appendChild(createItem(curlyFries,
             'curly fries',
             'Curly Fries',
             `Potatoes sliced with a curly fry cutter or spiralizer
-            and fried into ringlets.`));
-    menuCard.appendChild(createItem(shoeString,
-            'shoestring fries',
-            'Shoestring Fries',
-            `Potatoes sliced very thinly using a mandoline or
-            julienne cutter and fried extra-crisp.`));
-    menuCard.appendChild(createItem(crinkleCut,
-            'crinkle cut',
-            'Crinkle Cut',
-            `Potatoes thick-cut on a corrugated slicer, then sliced
-            into wavy batons and fried.`));
+            and fried into ringlets.`,
+            `Photo by Amanda Lim on Unsplash`));
+  
     menuCard.appendChild(createItem(cheeseFries,
             'cheese fries',
             'Cheese Fries',
-            `Any style of fries with cheese melted on top.`));
+            `Any style of fries with cheese melted on top.`,
+            `Photo by Erik Mclean on Unsplash`));
+
     menuCard.appendChild(createItem(steakFries,
             'steak fries',
             'Steak Fries',
             `Thick-cut fries usually double-fried to maximize
-            crispiness.`));
+            crispiness.`,
+            `Photo by nessip 77 on Unsplash`));
     menuCard.appendChild(createItem(chips,
             'chips (british)',
             'Chips (British)',
             `Shorter potatoes cut chunkier than standard fries,
-            traditionally served with malt vinegar.`));
-    menuCard.appendChild(createItem(sweetPotatoFries,
-            'sweet potato fries',
-            'Sweet Potato Fries',
-            `Any cut of fries made with sweet potatoes instead
-            of russet or Idaho.`));
+            traditionally served with malt vinegar.`,
+            `Photo by Farrah K on Unsplash`));
+    
     menuCard.appendChild(createItem(potatoWedges,
             'potato wedges',
             'Potato Wedge',
             `Potatoes cut into thick slices, then cut on an
-            angle to make triangular wedges.`));
-    menuCard.appendChild(createItem(ccFries,
-            'Chilli Cheese Fries',
-            'Chilli Cheese Fries',
-            `Any cut of fries topped with chilli and 
-            melted cheese.`));
-    menuCard.appendChild(createItem(cottageFries,
-            'Cottage Fries',
-            'Cottage Fries',
-            `Potatoes thick-cut on a corrugated slicer 
-            and fried.`));
-    menuCard.appendChild((pommesSouffles,
-            'Pommes Souffles',
-            'Pommes Souffles',
-            `Potatoes sliced medium-thin and fried once in
-            hot oil, then again in hotter oil until they
-            puff up.`));
+            angle to make triangular wedges.`,
+            `Photo by Nathan Dumlao on Unsplash`));
+   
     menuCard.appendChild(createItem(potatoTornado,
             'Potato Tornado',
             'Potato Tornado',
             `A whole potato on a skewer, thinly sliced 
             through  to make a spiral, fanned out and
-            deep-fried.`));
-    menuCard.appendChild(createItem(smiley,
-            'Smiley',
-            'Smiley',
-            `Potatoes cut into thick rounds with a happy
-            face stamped out and deep fried.`))
+            deep-fried.`,
+            `Photo by Suman Shrestha on Unsplash`));  
     
+    return menuCard;
 }
 
 export {menu};
