9b2485b37c0b566e5478d9a720fd0133be08eee0
Michael George <michael.g.george@gmail.com>
2022-07-29 01:00:28 +0530

    Refactor home-page to use createDiv helper

--- src/home-page.js
+++ src/home-page.js
@@ -1,13 +1,13 @@
-import { createParagraph, createImage } from "./helper";
+import { createParagraph, createImage, createDiv } from "./helper";
 import homeImage from './home-img.jpg';
 
 export default function homepage(){
-    const container = document.createElement('div');
+    const container = createDiv();
     
 
     container.appendChild(createParagraph("Fries to kill for!"));
     container.appendChild(createParagraph("Some copy about fries"));
-    container.appendChild(createImage(homeImage, "An Image of Fries"))
+    container.appendChild(createImage(homeImage, "An Image of Fries"));
 
     return container;
 }
