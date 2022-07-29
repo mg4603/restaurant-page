80596da6b68a9e067d9e5bd9b48b61875ea45763
Michael George <michael.g.george@gmail.com>
2022-07-30 02:35:01 +0530

    Add class selector to home-page and style it

--- src/home-page.js
+++ src/home-page.js
@@ -4,6 +4,7 @@ import homeImage from './images/home-img.jpg';
 export default function homepage(){
     const container = createDiv();
     
+    container.classList.add('home-page')
 
     container.appendChild(createParagraph("Fries to kill for!"));
     container.appendChild(createParagraph("Some copy about fries"));
