67cf1b7c904ed25196cb81d1679f8b0e2e8853be
Michael George <michael.g.george@gmail.com>
2022-07-29 06:39:30 +0530

    Complete nav-bar function

---
+++ src/nav.js
@@ -0,0 +1,25 @@
+import { createDiv, createUl, createLi} from "./helper";
+
+function navItem(text){
+    const item = createLi();
+
+    item.classList.add('nav-item');
+
+    item.textContent(text);
+
+    return item;
+}
+
+function nav(){
+    const navList = createUl();
+
+    navList.classList.add('nav');
+
+    navList.appendChild(navItem("Home"));
+    navList.appendChild(navItem("Menu"));
+    navList.appendChild(navItem("Contact"));
+
+    return navList;
+}
+
+export {nav};
