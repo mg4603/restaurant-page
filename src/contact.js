ccde0d100491ad958e5c01e3ba942dbab45871bf
Michael George <michael.g.george@gmail.com>
2022-07-29 06:12:01 +0530

    Trim unnecessary imports from contact page

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,4 @@
-import {createDiv, createH2, createParagraph, createImage} from './helper';
+import {createDiv,  createParagraph, createImage} from './helper';
 import emailIcon from './images/email.svg';
 import phoneIcon from './images/phone.svg';
 import mobileIcon from './images/cellphone.svg';
